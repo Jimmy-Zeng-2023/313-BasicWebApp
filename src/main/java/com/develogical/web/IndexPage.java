10a08b0917a19aaeb2ca520e796de9731a03f054
Jimmy Zeng <jimmyzen@andrew.cmu.edu>
2022-09-21 17:43:08 -0400

    Updated index page

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -8,10 +8,12 @@ public class IndexPage extends HtmlPage {
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
                 "<h1>Welcome!</h1>" +
-                "<p>Enter your query in the box below: " +
+                "<p>This is a querying app\n" +
+                  "Enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
                   "</form>" +
+                
                 "</p>");
     }
     
